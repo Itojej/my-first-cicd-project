18d0055d769f1f24d0713ef91cf6a43a89269c0a
JOSEPH ITOJE <J.ITOJE@OUTLOOK.COM>
2026-05-21 15:02:18 +0100

    fixed render deployment

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("CI/CD is working!");
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello CI/CD World");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
